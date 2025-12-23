b46df67cbf2bba0061b14ae67a32e2b70d7499e0
test <test@test.com>
2025-12-23 15:04:06 +0000

    argocd-play fix image version

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
 
+import com.sun.net.httpserver.HttpServer;
+
 import java.io.IOException;
+import java.net.InetSocketAddress;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -9,9 +12,8 @@ public class Main {
         System.out.println("Starting server...");
 
         // This opens the port so the Service has something to talk to
-        com.sun.net.httpserver.HttpServer server =
-                com.sun.net.httpserver.HttpServer.create(
-                        new java.net.InetSocketAddress(8080), 0);
+        // Force binding to 0.0.0.0 (all IPv4 interfaces)
+        HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", 8080), 0);
 
 
         // Main Business Logic
